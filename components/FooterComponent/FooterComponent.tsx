001554e2dc765e017aabd380c9692fb3e98d28a1
Tdibacco17 <tomasdibacco@gmail.com>
2023-04-29 22:02:56 -0300

    test

--- components/FooterComponent/FooterComponent.tsx
+++ components/FooterComponent/FooterComponent.tsx
@@ -1,10 +1,9 @@
-
 import styles from "./FooterComponent.module.scss"
 
 export default function FooterComponent() {
     return (
         <div className={styles["container-section-footer"]}>
-            footer
+            
         </div>
     )
 }
--- pages/test.tsx
+++ pages/test.tsx
@@ -4,7 +4,6 @@ export default function test() {
 
   return (
     <LayoutComponent isHomePage={false}>
-      
     </LayoutComponent>
   )
 }
