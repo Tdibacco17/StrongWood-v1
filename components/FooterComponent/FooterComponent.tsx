57b89754cb1bd00a54855eb59d99c8009aa158ca
Tomas Di Bacco <100334543+Tdibacco17@users.noreply.github.com>
2023-04-30 02:00:23 -0300

    commit

--- components/FooterComponent/FooterComponent.tsx
+++ components/FooterComponent/FooterComponent.tsx
@@ -11,10 +11,18 @@ export default function FooterComponent() {
                     © 2023 StrongWood | Todos los derechos reservados.
                 </h4>
                 <div className={styles["container-icons"]}>
-                    <FooterIconsComponent imgSrc="/assets/icons/facebook.svg" />
-                    <FooterIconsComponent imgSrc="/assets/icons/gmail.svg" />
-                    <FooterIconsComponent imgSrc="/assets/icons/instagram.svg" />
-                    <FooterIconsComponent imgSrc="/assets/icons/whatsapp.svg" />
+                    {/* <a href="https://www.facebook.com/strongwood_ar" target="_blank" aria-label="Facebook StrongWood">
+                        <FooterIconsComponent imgSrc="/assets/icons/facebook.svg" />
+                    </a> */}
+                    <a href="mailto:strongwoodventas@gmail.com" target="_blank" rel="noopener noreferrer" aria-label="Email StrongWood">
+                        <FooterIconsComponent imgSrc="/assets/icons/gmail.svg" />
+                    </a>
+                    <a href="https://instagram.com/strongwood_ar" target="_blank" rel="noopener noreferrer" aria-label="Instagram StrongWood">
+                        <FooterIconsComponent imgSrc="/assets/icons/instagram.svg" />
+                    </a>
+                    <a href="https://wa.me/5491133346111" target="_blank" rel="noopener noreferrer" aria-label="Whatsapp StrongWood">
+                        <FooterIconsComponent imgSrc="/assets/icons/whatsapp.svg" />
+                    </a>
                 </div>
             </div>
         </div>
--- components/HomeTwoCategoriesComponent/HomeTwoCategoriesComponent.tsx
+++ components/HomeTwoCategoriesComponent/HomeTwoCategoriesComponent.tsx
@@ -16,7 +16,8 @@ export default function HomeTwoCategoriesComponent() {
                         />
                     </div>
                     <div className={styles["container-overlay-image"]}>
-                        <h6 className={styles["title-overlay"]}> STRONG DESIGN </h6>
+                        <h6 className={styles["title-overlay"]}> Línea Design </h6>
+                        <h6 className={styles["subtitle-overlay"]}>Diseños exclusivos en amoblamientos</h6>
                     </div>
                 </div>
             </Link>
@@ -31,7 +32,8 @@ export default function HomeTwoCategoriesComponent() {
                         />
                     </div>
                     <div className={styles["container-overlay-image"]}>
-                        <h6 className={styles["title-overlay"]}> STRONG WOOD </h6>
+                        <h6 className={styles["title-overlay"]}> Línea Wood </h6>
+                        <h6 className={styles["subtitle-overlay"]}>El balance perfecto entre hierro y madera.</h6>
                     </div>
                 </div>
             </Link>
