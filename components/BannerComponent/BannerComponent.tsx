7fe99559de81c4b81ad5d262908a298b37ef9862
Tdibacco17 <tomas.dibacco@25watts.com.ar>
2023-05-03 21:15:51 -0300

    bannerImage responsive

--- components/BannerComponent/BannerComponent.tsx
+++ components/BannerComponent/BannerComponent.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
-import styles from "./BannerComponent.module.scss"
+import styles from "./BannerComponent.module.scss";
 import { CustomStyles } from "@/types/Interfaces";
+import { useWindowSize } from "@/utils/size/useWindowSIze";
 
 export default function BannerComponent({
     imgSrc,
@@ -8,27 +9,28 @@ export default function BannerComponent({
     originalWidth,
     originalHeight,
 }: {
-    imgSrc: string,
-    imgAlt: string,
-    originalWidth: number,
-    originalHeight: number,
+    imgSrc: string;
+    imgAlt: string;
+    originalWidth: number;
+    originalHeight: number;
 }) {
+    const { width, height } = useWindowSize();
+    const isPortrait = height > width;
 
     const newValues: CustomStyles = {
         "--original-width": `${originalWidth}`,
         "--original-height": `${originalHeight}`,
+        "--banner-image-inner-height": isPortrait ? "100vh" : "100vw",
+        "--banner-image-proportion": isPortrait
+            ? `calc((var(--banner-image-inner-height)) * (var(--original-height)) / (var(--original-width)))`
+            : `calc((var(--banner-image-inner-height)) * (var(--original-width)) / (var(--original-height)))`,
     };
 
     return (
         <div className={styles["container-outer-image"]} style={newValues}>
             <div className={styles["container-inner-image"]}>
-                <Image
-                    src={`${imgSrc}`}
-                    alt={`${imgAlt}`}
-                    fill
-                    priority
-                />
+                <Image src={imgSrc} alt={imgAlt} fill priority />
             </div>
         </div>
-    )
+    );
 }
