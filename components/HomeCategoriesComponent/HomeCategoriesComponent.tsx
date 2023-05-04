88aa850fb76575704fa8f191fd3a8d1f35fc95c4
Tdibacco17 <tomas.dibacco@25watts.com.ar>
2023-05-03 21:38:47 -0300

    fix | card homepage

--- components/HomeCategoriesComponent/HomeCategoriesComponent.tsx
+++ components/HomeCategoriesComponent/HomeCategoriesComponent.tsx
@@ -11,9 +11,9 @@ export default function HomeTwoCategoriesComponent() {
                     imgAlt="Imagen"
                     originalWidth={6048}
                     originalHeight={4024}
-                    outerHeight={30}
-                    outerWidth={30}
-                    size={30}
+                    outerHeight={32.5}
+                    outerWidth={32.5}
+                    size={32.5}
                     title="Línea Wood"
                     subtitle="El balance perfecto entre hierro y madera."
                 />
@@ -24,9 +24,9 @@ export default function HomeTwoCategoriesComponent() {
                     imgAlt="Imagen"
                     originalWidth={4016}
                     originalHeight={4016}
-                    outerHeight={30}
-                    outerWidth={30}
-                    size={30}
+                    outerHeight={32.5}
+                    outerWidth={32.5}
+                    size={32.5}
                     title="Línea Wood"
                     subtitle="El balance perfecto entre hierro y madera."
                 />
