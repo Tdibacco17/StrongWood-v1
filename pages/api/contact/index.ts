4b1f7f26f8047b60177f6fc76d16ba0f077eb2b7
Tdibacco17 <tomas.dibacco@25watts.com.ar>
2023-05-30 09:18:02 -0300

    feature | nodemailer

--- pages/api/contact/index.ts
+++ pages/api/contact/index.ts
@@ -1,27 +1,29 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import nodemailer from "nodemailer"
-require("dotenv").config();
+import nodemailer from "nodemailer";
+import dotenv from 'dotenv';
+dotenv.config();
 
 export default async function sendEmail(req: NextApiRequest, res: NextApiResponse) {
     const { name = "", phone = "", email = "", direction = "", product = "", pago = "" } = req.body;
 
     try {
         let transporter = nodemailer.createTransport({
-            host: "smtp-mail.outlook.com" || process.env.EMAIL_SERVICE,
-            port: 587,
+            host: process.env.EMAIL_HOST,
+            service: process.env.EMAIL_SERVICE,
             secure: false,
+            port: 587, // port for secure SMTP
             tls: {
                 ciphers: 'SSLv3'
             },
             auth: {
-                user: "tdibacco-prueba25watts@outlook.com" || process.env.EMAIL_USERNAME,
-                pass: "Tomasdibacco25watts" || process.env.EMAIL_PASSWORD,
+                user: process.env.EMAIL_USERNAME,
+                pass: process.env.EMAIL_PASSWORD,
             },
         });
 
-        let info = await transporter.sendMail({
-            from: "tdibacco-prueba25watts@outlook.com" || process.env.EMAIL_SENDER,
-            to: "tdibacco-prueba25watts@outlook.com" || process.env.EMAIL_SENDER,
+        let data = {
+            from: process.env.EMAIL_SENDER,
+            to: process.env.EMAIL_SENDER,
             subject: `${name} te envió un mensaje`,
             text: `
                 nombre: ${name}
@@ -31,9 +33,11 @@ export default async function sendEmail(req: NextApiRequest, res: NextApiRespons
                 producto: ${product}
                 abona en ${pago}
             `,
-        });
+        }
 
-        console.log('Message sent: ' + info.response);
+        const info = await transporter.sendMail(data);
+        console.log('Correo enviado:', info.response);
+
         return res.status(200).json({ message: "Correo electrónico enviado con éxito" });
     } catch (error) {
         console.log(error);
