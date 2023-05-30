21f162ed4cb8d9c4f094750208bb17ae7b787df2
Tdibacco17 <tomas.dibacco@25watts.com.ar>
2023-05-30 18:34:14 -0300

    feature | nodemailer config

--- pages/api/contact/index.ts
+++ pages/api/contact/index.ts
@@ -1,42 +1,65 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import nodemailer from "nodemailer";
+import { google } from "googleapis";
+
 import dotenv from 'dotenv';
 dotenv.config();
 
 export default async function sendEmail(req: NextApiRequest, res: NextApiResponse) {
     const { name = "", phone = "", email = "", direction = "", product = "", pago = "" } = req.body;
 
+    const contentHtml = `
+        <h2>STRONG WOOD</h2>
+        <h4>Datos personales</h4>
+        <div>
+            <ul>   
+                <li>Nombre: ${name}</li>
+                <li>Email: ${email}</li>
+                <li>Telefono: ${phone}</li>
+                <li>Dirección: ${direction}</li>  
+            </ul>
+        </div>
+        <br></br>
+        <ul>
+            <li>Quiere comprar el producto: <strong>${product}</strong></li>
+            <li>Abona en <strong>${pago}</strong></li>
+        </ul>
+    `
+
+    const CLIENT_ID = process.env.CLIENT_ID;
+    const CLIENT_SECRET = process.env.CLIENT_SECRET;
+    const REDIRECT_URI = process.env.REDIRECT_URI;
+    const REFRESH_TOKEN = process.env.REFRESH_TOKEN;
+
+    const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI)
+    oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
+
     try {
+        const accessToken = await oAuth2Client.getAccessToken()
+
         let transporter = nodemailer.createTransport({
-            host: process.env.EMAIL_HOST,
+            // @ts-ignore
             service: process.env.EMAIL_SERVICE,
-            secure: false,
-            port: 587, // port for secure SMTP
-            tls: {
-                ciphers: 'SSLv3'
-            },
             auth: {
+                type: "OAuth2",
                 user: process.env.EMAIL_USERNAME,
-                pass: process.env.EMAIL_PASSWORD,
+                clientId: CLIENT_ID,
+                clientSecret: CLIENT_SECRET,
+                refreshToken: REFRESH_TOKEN,
+                accessToken: accessToken
             },
         });
 
-        let data = {
-            from: process.env.EMAIL_SENDER,
+        const mailOptions = {
+            from: `STRONG WOOD <${process.env.EMAIL_USERNAME}>`,
             to: process.env.EMAIL_SENDER,
-            subject: `${name} te envió un mensaje`,
-            text: `
-                nombre: ${name}
-                email: ${email}
-                telefono: ${phone}
-                direccion: ${direction}
-                producto: ${product}
-                abona en ${pago}
-            `,
+            subject: "Nueva venta",
+            // text: "prueba",
+            html: contentHtml,
         }
 
-        const info = await transporter.sendMail(data);
-        console.log('Correo enviado:', info.response);
+        const result = await transporter.sendMail(mailOptions)
+        console.log('Correo enviado:', result.response);
 
         return res.status(200).json({ message: "Correo electrónico enviado con éxito" });
     } catch (error) {
