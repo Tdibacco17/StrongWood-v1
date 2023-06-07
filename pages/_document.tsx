8484e339e0e6a3b886bebc785c0b5106b4c79147
Tomas Di Bacco <100334543+Tdibacco17@users.noreply.github.com>
2023-06-07 12:11:41 -0300

    fix | preload fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,26 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          href="/assets/fonts/Knucklehead.otf"
+          rel="preload"
+          as="font"
+          crossOrigin=""
+        />
+        <link
+          href="/assets/fonts/Roboto-Bold.ttf"
+          rel="preload"
+          as="font"
+          crossOrigin=""
+        />
+        <link
+          href="/assets/fonts/Roboto-Regular.ttf"
+          rel="preload"
+          as="font"
+          crossOrigin=""
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
