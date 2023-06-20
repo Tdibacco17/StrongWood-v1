5fbd86f2895beea93f1648d89b95fa59b1b6bd5e
Tdibacco17 <tomasdibacco@gmail.com>
2023-06-20 20:36:19 -0300

    todo: subcategorie

---
+++ components/DesignDetailCardComponent/DesignDetailCardComponent.tsx
@@ -0,0 +1,10 @@
+import { ImgDataInterface } from "@/types/Interfaces"
+import styles from "./DesignDetailCardComponent.module.scss"
+
+export default function DesignDetailCardComponent({ imgData }: { imgData: ImgDataInterface }) {
+    return (
+        <>
+            DesignDetailCardComponent
+        </>
+    )
+}
--- components/DesignDetailComponent/DesignDetailComponent.tsx
+++ components/DesignDetailComponent/DesignDetailComponent.tsx
@@ -1,7 +1,8 @@
 import { DesignDetailContext } from "@/context/DesignDetailProvider"
-import { DesignDataContextInterface } from "@/types/Interfaces"
-import { useContext } from "react"
+import { DesignDataContextInterface, ImgDataInterface, SubCategorieDataInterface } from "@/types/Interfaces"
+import React, { useContext } from "react"
 import styles from "./DesignDetailComponent.module.scss"
+import DesignDetailCardComponent from "../DesignDetailCardComponent/DesignDetailCardComponent"
 
 export default function DesignDetailComponent() {
     const { designData } = useContext(
@@ -11,7 +12,25 @@ export default function DesignDetailComponent() {
     console.log(designData)
     return (
         <div className={styles["container-section-subcategories"]}>
-            {designData && designData.title}
+            <div className={styles["container-subcategories"]}>
+                {designData &&
+                    designData.subcategories.map((e: SubCategorieDataInterface) => {
+                        return (
+                            <div key={e.title} className={styles["subcategorie"]}>
+                                <p>{e.title}</p>
+                                <div className={styles["subcategorie-images"]}>
+                                    {e.images.map((imgData: ImgDataInterface) => {
+                                        return (
+                                            <React.Fragment key={imgData.imgSlug}>
+                                                <DesignDetailCardComponent imgData={imgData} />
+                                            </React.Fragment>
+                                        )
+                                    })}
+                                </div>
+                            </div>
+                        )
+                    })}
+            </div>
         </div>
     )
 }
