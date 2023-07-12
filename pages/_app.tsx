adbb4d437bd07d4529edb976c10e199c48eb7471
Tdibacco17 <tomas.dibacco@25watts.com.ar>
2023-07-12 15:15:47 -0300

    fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,22 @@
 import { ContactProvider } from '@/context/ContactContextProvider'
 import '@/globals.css'
 import type { AppProps } from 'next/app';
+import localFont from 'next/font/local';
+
+const Knucklehead = localFont({
+  src: '../public/assets/fonts/Knucklehead.otf',
+  variable: '--font-Knucklehead',
+  display: "optional",
+  weight: "700",
+  style: "normal"
+})
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ContactProvider>
-      <Component {...pageProps} />
-    </ContactProvider>
+    <main className={`${Knucklehead.className}`}>
+      <ContactProvider>
+        <Component {...pageProps} />
+      </ContactProvider>
+    </main>
   )
 }
